c9d7ac8efa828ce49527c59d40fabf6a1efa592b
kuriyantoadi <melatikuning212@gmail.com>
2022-02-17 22:27:17 +0700

    update 17 feb 2022, update 2

--- code_arduino/code_arduino.ino
+++ code_arduino/code_arduino.ino
@@ -1,28 +1,28 @@
-#include <Servo.h> // menyertakan library servo ke dalam program 
+#include <Servo.h> // menyertakan library servo ke dalam program
 Servo myservo;     // variable untuk menyimpan posisi data
-int pos = 00;   
-         
-void setup(){ 
+int pos = 00;
+
+void setup(){
  // myservo.attach(1); //sinyal data kabel motor servo dikonekan di pin 3 Arduino
-} 
- 
-void loop(){ 
+}
+
+void loop(){
  delay(1000); //delay di bawah
-  
+
  for(pos = 00; pos < 180; pos += 1)  //fungsi perulangan yang akan dijadikan PWM dengan kenaikan 1
  {
   myservo.write(pos); //prosedur penulisan data PWM ke motor servo
-  delay(15); //waktu tunda 15 ms       titik A          
- } 
- 
- delay(1000); //delay di bawah
- 
- for(pos = 180; pos>=1; pos-=1)  //fungsi perulangan yang akan dijadikan PWM dengan penurunan 1
- {                              
-  myservo.write(pos);                 
-  delay(2);                      // titik B  
+  delay(15); //waktu tunda 15 ms       titik A
  }
- 
+
+ delay(1000); //delay di bawah
+
+ for(pos = 180; pos>=1; pos-=1)  //fungsi perulangan yang akan dijadikan PWM dengan penurunan 1
+ {
+  myservo.write(pos);
+  delay(2);                      // titik B
+ }
+
   delay(2000); //delay di atas
 
 }
